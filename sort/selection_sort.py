f6946c48d1b2a29676ce9014c36e615da4a9f1c9
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-08-30 21:31:16 -0300

    selection sort

---
+++ sort/selection_sort.py
@@ -0,0 +1,9 @@
+class SelectionSort:
+    @staticmethod
+    def sort(self, data):
+        for i in range(len(data)):
+            min_index = i
+            for j in range(i + 1, len(data)):
+                if data[j] < data[min_index]:
+                    min_index = j
+            data[i], data[min_index] = data[min_index], data[i]
