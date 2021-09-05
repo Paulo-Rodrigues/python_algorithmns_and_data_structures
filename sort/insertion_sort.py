dd6b980fb5f5f1890700712f7ee6e856c3e6eee0
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-09-04 22:50:45 -0300

    insertion sort

---
+++ sort/insertion_sort.py
@@ -0,0 +1,9 @@
+class InsertionSort:
+  @staticmethod
+  def sort(array):
+    for i in range(1, len(array)):
+      j = i
+      while j > 0 and array[j] < array[j-1]:
+        array[j], array[j-1] = array[j-1], array[j]
+        j -= 1
+    return array
